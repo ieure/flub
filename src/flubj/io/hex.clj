095b6b3cd08defba90c120eeb2c177574e678a8b
Ian Eure <ian.eure@gmail.com>
2013-12-08 06:35:19 +0000

    Small cleanup

--- src/flubj/io/hex.clj
+++ src/flubj/io/hex.clj
@@ -57,8 +57,8 @@
                                (many1 (token #{\0 \1 \2 \3 \4 \5 \6
                                                \7 \8 \9 \A \B \C \D
                                                \E \F}))
-                               (choice (attempt (eof))
-                                       (attempt (char \*))
+                               (choice (eof)
+                                       (char \*)
                                        (any-char))))]
           (always (= \* cont))))
 
