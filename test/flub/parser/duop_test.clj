722c416b48580bd460877142feea3f58a6d6352a
Ian Eure <ian.eure@gmail.com>
2013-12-10 06:18:51 +0000

    duop.

---
+++ test/flub/parser/duop_test.clj
@@ -0,0 +1,12 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2013 Buster Marx, Inc All rights reserved.
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flub.parser.duop-test
+  (:use [clojure.test]
+        [the.parsatron :only [run any-char >>]])
+  (:require [flub.parser.duop :as d]))
+
+(deftest test-duop
+  (is (= [:or [:term [:register 10] [[:inc 1]]] [:term 127]] (run d/duop "REGA INC OR 7F"))))
