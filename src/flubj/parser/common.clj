b52772efb0fc96c0c91e56a72c1fa54408965896
Ian Eure <ian.eure@gmail.com>
2013-12-10 04:06:33 +0000

    Common parsing stuff.

---
+++ src/flubj/parser/common.clj
@@ -0,0 +1,37 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2013 Buster Marx, Inc All rights reserved.
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flubj.parser.common
+  (:refer-clojure :exclude [char comment])
+  (:use [the.parsatron])
+  (:require [clojure.string :as cstr]))
+
+(defparser butchar
+  ;;  "Match any token except `c'."
+  [c]
+  (token #(not= c %)))
+
+;; Might be useful?
+#_(defparser optional [p]
+    (either (attempt b)
+            (always nil)))
+
+(defn- string* [& s]
+  (map string s))
+
+(defparser mchoice
+  ;;  "Multiple-choice; attempt each of `parsers' in turn."
+  [& parsers]
+  (apply choice (map attempt parsers)))
+
+(defparser mchoice*
+  ;;  "Multiple-choice; attempt each of `parsers' in turn."
+  [& parsers]
+  (many (apply mchoice parsers)))
+
+(defn kw  "Convert a string to a keyword." [s]
+  (-> (cstr/lower-case s)
+      (cstr/replace #" +" "-")
+      (keyword)))
