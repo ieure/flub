4b71a0379a78b93811783b8bc95c75b19d71ceef
Ian Eure <ian.eure@gmail.com>
2014-01-20 08:56:12 -0800

    Use `flub.io.ws`.

--- src/flub/parser/source.clj
+++ src/flub/parser/source.clj
@@ -1,19 +1,15 @@
 ;; -*- coding: utf-8 -*-
 ;;
-;; © 2013 Buster Marx, Inc All rights reserved.
+;; © 2013, 2014 Ian Eure
 ;; Author: Ian Eure <ian.eure@gmail.com>
 ;;
 (ns flub.parser.source
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]
-            [clojure.string :as string]))
-
-(def ^:constant eol #"(\r\n|\n|\r)")
+            [clojure.string :as string])
+  (:use [flub.io.ws]))
 
 (def p (insta/parser (slurp (io/resource "source.ebnf"))))
-
-(defn normalize-newlines [inp]
-  (string/replace inp eol "\n"))
 
 (defn normalize [inp]
   (-> (normalize-newlines inp)
