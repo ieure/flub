83a07b344dcd47a7b58fb22157fa669df2c3f1c8
Ian Eure <ian.eure@gmail.com>
2014-01-21 20:40:15 -0800

    First pass at the assembler, it seems to mostly work.

---
+++ src/flub/parser/asm.clj
@@ -0,0 +1,233 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2014 Ian Eure.
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flub.parser.asm
+  (:refer-clojure :exclude [resolve])
+  (:require [flub.parser.keys :as k]
+            [clojure.string :as string])
+  (:use [flub.io.bytes :only [string->bytes]]
+        [slingshot.slingshot :only [throw+]]
+        [clojure.core.match :only [match]]
+        [clojure.walk :only [prewalk]]
+        [clojure.tools.macro :only [macrolet]]))
+
+
+
+(declare emit)
+
+(defn vcc "Concatenates & flattens its arguments."
+  [& seqs] (vec (flatten seqs)))
+
+(defn resolve [tbl s]
+  (let [n (.indexOf (or tbl []) s)]
+    (if (< n 0)
+      (throw (NoSuchFieldException.
+              (format "Symbol `%s' does not exist in table: %s" s tbl)))
+      n)))
+
+(defn resolve-prog "Resolve a program reference."
+  [{:keys [progs] :as state} prog]
+  (match prog
+         [:SYMBOL s] (resolve progs s)
+         :else (emit state prog)))
+
+(defn resolve-label "Resolve a label."
+  [{:keys [labels] :as state} label]
+  (match label
+         [:SYMBOL s] (resolve labels s)
+         :else (emit state label)))
+
+
+
+(defn scan-progs "Extract programs from AST"
+  [ast]
+  (filterv #(match % [:PROGRAM & p] p :else nil) ast))
+
+(defn progname "Extract program name" [prog-ast]
+  (->> (map #(match % [:PROGRAM_HEAD [:SYMBOL s]] s :else nil) prog-ast)
+       (drop-while nil?)
+       (first)))
+
+(defn- scan-prognames* [defs uses elt]
+  (match elt
+         [:PROGRAM_HEAD [:SYMBOL s]] (conj! defs s)
+         [:EXECUTE [:SYMBOL s]]      (conj! uses s)
+         :else nil)
+  elt)
+
+(defn scan-prognames [ast]
+  (let [defs (transient [])
+        uses (transient [])]
+    (prewalk #(scan-prognames* defs uses %) ast)
+    {:defs (persistent! defs)
+     :uses (persistent! uses)}))
+
+ ;; Labels
+
+(defn- scan-labels* [defs uses elt]
+  (match elt
+         [:LABEL [:SYMBOL s]] (conj! defs s)
+         [:GOTO [:SYMBOL s]]  (conj! uses s)
+         :else nil)
+  elt)
+
+(defn scan-labels [ast]
+  (let [defs (transient [])
+        uses (transient [])]
+    (prewalk #(scan-labels* defs uses %) ast)
+    {:defs (persistent! defs)
+     :uses (persistent! uses)}))
+
+
+;; Emitting bytes
+;;
+;; The byte emitter is built with multimethods which dispatch to the
+;; correct emitter for that part of the AST. The emit multifn takes
+;; two arguments, `state' and `ast'. State contains a map containing
+;; program symbols and label symbols for the current program. The
+;; :PROGRAM and :S methods push new info into the state, and most
+;; other methods don't care. The defemit and with-thunk macros handle
+;; passing state between emitters. If an emitter needs to call
+;; another, calling `(thunk ast)' will automatically inject the
+;; current state into the call. Emitters which need to alter state
+;; must do so manually.
+
+(defn emit-dispatch
+  "Dispatch an emit call.
+
+   If there is only one arg, assume there is no state and dispatch to
+   :stateless. This lets us do arity overloading in the multimethod."
+  [& args]
+  (cond
+   (= 1 (count args)) :stateless
+   true               (ffirst (next args))))
+
+(def ^:constant no-state {:stack [] :labels [] :progs []})
+
+(defmulti emit "Emit bytes for the AST" emit-dispatch)
+
+;; For cases where no state is passed in
+(defmethod emit :stateless [ast]
+  (emit no-state ast))
+
+(defmacro with-thunk [& body]
+  `(macrolet [(~'thunk [ast#] `(emit ~'state ~ast#))]
+             ~@body))
+
+(defmacro defemit "Define an AST emitter."
+  [kw args & body]
+  `(defmethod emit ~kw ~args
+     (let [~'state (update-in ~'state [:stack] conj ~kw)
+           stack# (:stack ~'state)]
+       (try
+         ~@body
+         (catch Exception e#
+           (throw+ {:stack stack#
+                    :exception e#}))))))
+
+;; Fallback emitter - this will break things pretty badly.
+(defemit :default [{:keys [stack] :as state} [s & _]]
+  ;; FIXME for now. Once things are in a better state, it should throw
+  ;; an exception.
+  [:FIXME {:stack stack
+           :terminal s}])
+
+(defemit :REGISTER [state [_ reg-or-sym]]
+  (k/keys :reg reg-or-sym))
+
+(defemit :HEX [state [h vals]]
+  (mapv k/key vals))
+
+(defemit :DEC [state [d vals]]
+  (mapv k/key vals))
+
+(def ^:constant unop-map
+  {"CPL" 'compl
+   "DEC" 'decr
+   "INC" 'incr
+   "SHL" 'shift-left
+   "SHR" 'shift-right})
+
+(defemit :UNARY [state [_ op target]]
+  (vcc (k/keys (get unop-map op))
+       (emit state target)))
+
+(defemit :DISPLAY [state [d sast]]
+  (vcc (k/key 'displ)
+       (emit state sast)))
+
+(defemit :STRING [state [s sval]]
+  ;; FIXME - need to replace symbol names
+  (string->bytes sval))
+
+(defemit :GOTO [{:keys [labels] :as state} [g label]]
+  (vcc (k/key 'goto)
+       (resolve-label state label)))
+
+;; The start of the source scans for progam symbols and pushes them
+;; into the state
+(defemit :S [state [s & rest]]
+  (map (partial emit (assoc state :progs (:defs (scan-prognames rest))))
+       rest))
+
+;; The start of a program scans for progam labels and pushes them into
+;; the state
+(defemit :PROGRAM [state [p & rest]]
+  (map (partial emit (assoc state :labels (:defs (scan-labels rest)))) rest))
+
+;; Containers - these just emit their contents
+
+(defemit :STATEMENT [state [s & rest]]
+  (mapv (partial emit state) rest))
+
+(defemit :TERM [state [p & rest]]
+  (map (partial emit state) rest))
+
+(defemit :EXPR [state [p & rest]]
+  (map (partial emit state) rest))
+
+(defemit :ADDR [state [a rest]]
+  (vec (flatten (emit state rest))))
+
+(defemit :PROGRAM_HEAD [state [ph prog]]
+  (vcc 0x1a (resolve-prog state prog)))
+
+(defemit :PROGRAM_BODY [state [p & rest]]
+  ;; "0x53 indicates the start-of-program, and is always the first
+  ;; byte in a program."
+  ;; "0x50 indicates the end-of-program, and is always the last byte
+  ;; of a program (labels may follow)."
+  ;;
+  ;; - 9010A Programming Manual p. 7-5
+  (vcc [0x53]
+       (map (partial emit state) rest)
+       [0x50]))
+
+(defemit :LABEL [state [l label]]
+  (vcc (k/key :label)
+       (resolve-label state label)))
+
+(defemit :EXECUTE [state [e prog]]
+  (vcc (k/key :exec)
+       (resolve-prog state prog)))
+
+(defemit :WRITE [state [w target _ val]]
+  (vcc (k/key :exec)
+       (emit state target)
+       (k/key :=)
+       (emit state val)))
+
+(defemit :IF [state [i expr-a cond expr-b goto]]
+  (vcc (k/key :if)
+       (emit state expr-a)
+       (map k/key cond)
+       (emit state expr-b)
+       (emit state goto))
+  )
+
+ ;; User-servicable parts
+
+(defn ast->bytes "Emit bytes for an AST."
+  [ast] (emit ast))
---
+++ test/flub/parser/asm_test.clj
@@ -0,0 +1,154 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2014 Ian Eure
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flub.parser.asm-test
+  (:use [clojure.test]
+        [clojure.pprint])
+  (:require [flub.parser.asm :as a]
+            [flub.parser.source :as s]))
+
+#_(defonce ast
+    (s/source->ast (slurp "/Users/ieure/Dropbox/arcade/fluke/blit.s")))
+
+(defonce ast
+  [:S
+   [:PROGRAM
+    [:PROGRAM_HEAD [:SYMBOL "BLITUI"]]
+    [:PROGRAM_BODY
+     [:STATEMENT [:DISPLAY [:STRING "START ADDRESS @ /A"]]]
+     [:STATEMENT [:DISPLAY [:STRING "END ADDRESS @ /B"]]]
+     [:STATEMENT [:DISPLAY [:STRING "VALUE = /C"]]]
+     [:STATEMENT [:EXECUTE [:SYMBOL "BLIT"]]]]]
+
+   [:PROGRAM
+    [:PROGRAM_HEAD [:SYMBOL "BLIT"]]
+    [:PROGRAM_BODY
+     [:STATEMENT [:LABEL [:SYMBOL "WRITE"]]]
+     [:STATEMENT
+      [:WRITE
+       [:ADDR [:EXPR [:TERM [:REGISTER "A"]]]]
+       "="
+       [:EXPR [:TERM [:REGISTER "C"]]]]]
+     [:STATEMENT
+      [:IF
+       [:EXPR [:TERM [:REGISTER "A"]]]
+       ">="
+       [:EXPR [:TERM [:REGISTER "B"]]]
+       [:GOTO [:SYMBOL "DONE"]]]]
+     [:STATEMENT [:UNARY "INC" [:REGISTER "A"]]]
+     [:STATEMENT [:GOTO [:SYMBOL "WRITE"]]]
+     [:STATEMENT
+      [:LABEL [:SYMBOL "DONE"]]
+      [:STATEMENT [:DISPLAY [:STRING "DONE"]]]]]]])
+
+(defonce ast2
+  [:S
+   [:PROGRAM
+    [:PROGRAM_HEAD [:DEC "0"]]
+    [:PROGRAM_BODY
+     [:STATEMENT [:DISPLAY [:STRING "START ADDRESS @ /A"]]]
+     [:STATEMENT [:DISPLAY [:STRING "END ADDRESS @ /B"]]]
+     [:STATEMENT [:DISPLAY [:STRING "VALUE = /C"]]]
+     [:STATEMENT [:EXECUTE [:DEC "1"]]]]]
+   [:PROGRAM
+    [:PROGRAM_HEAD [:DEC "1"]]
+    [:PROGRAM_BODY
+     [:STATEMENT [:LABEL [:HEX "1"]]]
+     [:STATEMENT
+      [:WRITE
+       [:ADDR [:EXPR [:TERM [:REGISTER "A"]]]]
+       "="
+       [:EXPR [:TERM [:REGISTER "C"]]]]]
+     [:STATEMENT
+      [:IF
+       [:EXPR [:TERM [:REGISTER "A"]]]
+       ">="
+       [:EXPR [:TERM [:REGISTER "B"]]]
+       [:GOTO [:HEX "2"]]]]
+     [:STATEMENT [:UNARY "INC" [:REGISTER "A"]]]
+     [:STATEMENT [:GOTO [:HEX "1"]]]
+     [:STATEMENT
+      [:LABEL [:HEX "2"]]
+      [:STATEMENT [:DISPLAY [:STRING "DONE"]]]]]]])
+
+(defonce ast3
+  [:S
+   [:PROGRAM
+    [:PROGRAM_HEAD [:SYMBOL "BLIT"]]
+    [:PROGRAM_BODY
+     [:STATEMENT [:LABEL [:SYMBOL "WRITE"]]]
+     [:STATEMENT
+      [:WRITE
+       [:ADDR [:EXPR [:TERM [:REGISTER "A"]]]]
+       "="
+       [:EXPR [:TERM [:REGISTER "C"]]]]]
+     [:STATEMENT
+      [:IF
+       [:EXPR [:TERM [:REGISTER "A"]]]
+       ">="
+       [:EXPR [:TERM [:REGISTER "B"]]]
+       [:GOTO [:SYMBOL "DONE"]]]]
+     [:STATEMENT [:UNARY "INC" [:REGISTER "A"]]]
+     [:STATEMENT [:GOTO [:SYMBOL "WRITE"]]]
+     [:STATEMENT
+      [:LABEL [:SYMBOL "DONE"]]
+      [:STATEMENT [:DISPLAY [:STRING "DONE"]]]]]]])
+
+(deftest test-vcc
+  (is (= [1 2 3 4] (a/vcc [1 2] [3 4])))
+  (is (= [1 2 3 4] (a/vcc 1 2 [3 4])))
+  (is (= [1 2 3 4] (a/vcc '(1 2) [3 4]))))
+
+(deftest test-scan-progs
+  (is (= [[:PROGRAM 'A] [:PROGRAM 'B]]
+         (a/scan-progs [:S [:PROGRAM 'A] [:THING] [:PROGRAM 'B]]))))
+
+(deftest test-ast->bytes
+  (pprint (a/ast->bytes ast3)))
+
+(deftest test-emit-hex
+  (is (= [1 0xF 0xF 0xF] (a/emit [:HEX "1FFF"]))))
+
+(deftest test-emit-dec
+  (is (= [1 0 0 0] (a/emit [:DEC "1000"]))))
+
+(deftest test-emit-display
+  (is (= [62 211 212 193 210 212 160 193 196 196 210 197 211 211 160
+          192 160 175 193]
+         (a/emit [:DISPLAY [:STRING "START ADDRESS @ /A"]]))))
+
+(deftest test-emit-display-statement
+  (is (= [[62 211 212 193 210 212 160 193 196 196 210 197 211 211 160
+          192 160 175 193]]
+         (a/emit [:STATEMENT [:DISPLAY [:STRING "START ADDRESS @ /A"]]]))))
+
+(deftest test-emit-register
+  (is (= [56 10] (a/emit [:REGISTER "A"]))))
+
+(deftest test-emit-label-literal
+  (is (= [43 2] (a/emit [:LABEL [:HEX "2"]]))))
+
+(deftest test-emit-label-sym
+  (is (= [43 1] (a/emit {:labels ["ONE" "TWO"]}
+                        [:LABEL [:SYMBOL "TWO"]]))))
+
+(deftest test-emit-addr
+  (is (= [56 10] (a/emit [:ADDR [:EXPR [:TERM [:REGISTER "A"]]]]))))
+
+(deftest test-emit-unop
+  (is (= [52                            ; INC
+          56                            ; REG
+          10]                           ; A
+       (a/emit [:UNARY "INC" [:REGISTER "A"]]))))
+
+(deftest test-emit-program-head-literal
+  (is (= [0x1A                          ; Program start
+          0x01]                         ; #1
+         (a/emit [:PROGRAM_HEAD [:DEC "1"]]))))
+
+(deftest test-emit-program-head-symbol
+  (is (= [0x1A                          ; Program start
+          0x00]                         ; #0
+         (a/emit {:progs ["BLIT"]} [:PROGRAM_HEAD [:SYMBOL "BLIT"]]))))
