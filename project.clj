92992783f9f8823e75f47e3f2252bb09c51c69a6
Ian Eure <ian.eure@gmail.com>
2014-01-21 20:39:27 -0800

    Update deps, version, build args, :main, jvm opts.

--- project.clj
+++ project.clj
@@ -4,4 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [the/parsatron "0.0.3"]])
+                 [org.clojure/core.match "0.2.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/tools.macro "0.1.2"]
+                 [slingshot "0.10.3"]
+                 [clj-mmap "1.1.2"]
+                 [the/parsatron "0.0.3"]
+                 [instaparse "1.2.12"]]
+  :warn-on-reflection true
+  :aot :all
+  :jvm-opts ["-Xmx1g" "-Xms1g"]
+  :main flub.core)
