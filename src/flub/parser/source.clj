38eb6a655e3f1595aedf662723064b5bf3b1e3d9
Ian Eure <ian.eure@gmail.com>
2014-05-18 13:20:08 -0700

    include parsing

--- src/flub/parser/source.clj
+++ src/flub/parser/source.clj
@@ -6,8 +6,12 @@
 (ns flub.parser.source
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]
-            [clojure.string :as string])
-  (:use [flub.io.ws]))
+            [clojure.string :as string]
+            [clojure.walk :as walk])
+  (:use [flub.io.ws]
+        [clojure.pprint]
+        [clojure.core.match :only [match]])
+  (:import [java.io IOException]))
 
 (def p "Fluke source parser"
   (insta/parser (slurp (io/resource "source.ebnf"))))
@@ -18,5 +22,49 @@
   (-> (normalize-newlines inp)
       (string/upper-case)))
 
-(defn source->ast "Parse input and return an AST"
-  [^String inp] (p (normalize inp)))
+(declare pp-include)
+
+(def ^:dynamic *stack* [])
+
+(defn source->ast "Parse input and return an AST" [^String inp]
+  (with-meta
+    (->> (normalize inp)
+         (p)
+         #_(pp-include))
+    {:input nil}))
+
+(defn file->ast [file]
+  (printf "Parsing `%s'\n" file)
+  (binding [*stack* (conj *stack* file)]
+    (with-open [i (io/reader file)]
+      (with-meta
+        (doall (source->ast (slurp i)))
+        {:input file}))))
+
+ ;; Includes
+
+(defn- resource-include "Find an include file in the resources."
+  [name] (io/resource (str "include/" name)))
+
+(defn- pwd-include "Find an include file in the PWD." [name]
+  (let [cwdf (io/as-file (str "./" name))]
+    (when (.exists cwdf) cwdf)))
+
+(defn- find-include "Locate an include file." [name]
+  (printf "Locating include: `%s'\n" name)
+  (if-let [f (first (filter identity [(resource-include name)
+                                      (pwd-include name)]))]
+    (do
+      (printf "Found: `%s' -> `%s'\n" name f)
+      f)
+    (do
+      (printf "Failed to find: `%s'\n" name)
+      (throw (IOException. (format "Cannot find include file `%s'" name))))))
+
+(defn pp-include "Splice included files into the AST." [ast]
+  (walk/prewalk
+   (fn [form]
+     (match form
+            [:INCLUDE name] (file->ast (find-include name))
+            :else form))
+   ast))
