48146de2f23337acab2c0d05972cf0b5650b7468
Ian Eure <ian.eure@gmail.com>
2014-05-18 17:24:42 -0700

    Parse .POD files

---
+++ src/flub/parser/pod.clj
@@ -0,0 +1,29 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2013, 2014 Ian Eure
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flub.parser.pod
+  (:require [instaparse.core :as insta]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.walk :as walk])
+  (:use [flub.io.ws]
+        [clojure.pprint]
+        [clojure.core.match :only [match]])
+  (:import [java.io IOException]))
+
+(def p "Fluke pod definition parser"
+  (insta/parser (slurp (io/resource "pod.ebnf"))))
+
+(defn source->ast "Parse input and return an AST" [^String inp]
+  (with-meta
+    (p (normalize inp))
+    {:input inp}))
+
+(defn file->ast [file]
+  (printf "Parsing POD `%s'\n" file)
+  (with-open [i (io/reader file)]
+    (with-meta
+      (doall (source->ast (slurp i)))
+      {:input file})))
---
+++ test/flub/pod_test.clj
@@ -0,0 +1,19 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2014 Ian Eure
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flub.pod-test
+  (:use [clojure.test]
+        [clojure.pprint])
+  (:require [clojure.java.io :as io]
+            [flub.io.ws :as ws]
+            [flub.parser.pod :as pod]))
+
+(deftest test-pod-parse
+  (is (= [:S
+          [:FORCELN [:FORCE_LINE "BUSRQ"] "=" [:DEC "4"]]
+          [:FORCELN [:FORCE_LINE "WAIT"] "=" [:DEC "5"]]
+          [:BUS_ADDR [:HEX "FFFF"]]
+          [:UUT_ADDR [:HEX "0000"]]]
+         (pod/source->ast (slurp (io/resource "include/Z80.POD"))))))
