3e29f4ae69e2c078ff56e6c330ea4b77130a346b
Ian Eure <ian.eure@gmail.com>
2014-05-18 22:07:10 -0700

    Resolve symbols.

---
+++ src/flub/parser/symbols.clj
@@ -0,0 +1,40 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2014 Ian Eure
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flub.parser.symbols
+  (:require [clojure.walk :as walk])
+  (:use [clojure.core.match :only [match]]
+        [clojure.pprint]))
+
+(defn- declarations? [ast]
+  (match ast [:DECLARATIONS & _] true :else nil))
+
+(defn strip-decls [form]
+  (vec (if (coll? form) (remove declarations? form) form)))
+
+(defn- resolve-syms [ast]
+  (let [symtab (transient {})]
+    (walk/postwalk
+     (fn [form]
+       (match form
+              [:DECL_ASSIGN reg [:SYMBOL s]] (assoc! symtab [:SYMBOL s] reg)
+              [:DECL_ASSIGN reg [:SYMBOLS & ss]]
+              (doseq [s ss] (assoc! symtab s reg))
+
+              ;; Substitute reference, strip decls
+              :else (if (coll? form)
+                      (strip-decls (or (get symtab form) form))
+                      form)))
+     ast)))
+
+(defn process "Process symbols in a program source AST"
+  [ast]
+  (walk/postwalk
+   (fn [form]
+     (match form
+            [:PROGRAM & _] (resolve-syms form)
+            [:S & _] (resolve-syms form)
+            :else form))
+   ast))
---
+++ test/flub/parser/symbols_test.clj
@@ -0,0 +1,55 @@
+;; -*- coding: utf-8 -*-
+;;
+;; © 2014 Ian Eure
+;; Author: Ian Eure <ian.eure@gmail.com>
+;;
+(ns flub.parser.symbols-test
+  (:use [clojure.test]
+        [clojure.pprint])
+  (:require [flub.parser.symbols :as syms]
+            [flub.parser.source :as src]))
+
+(deftest resolve-syms
+  (is (= [:S [:INC [:REGISTER "A"]]]
+         (#'syms/resolve-syms
+          [:S
+           [:DECLARATIONS
+            [:DECL_ASSIGN [:REGISTER "A"] [:SYMBOL "CNTR"]]
+            [:DECL_ASSIGN [:REGISTER "2"] [:SYMBOLS [:SYMBOL "LEFT"]
+                                           [:SYMBOL "RIGHT"]]]]
+           [:INC [:SYMBOL "CNTR"]]]))))
+
+(deftest process
+    (let [code "
+DECLARATIONS
+    ASSIGN REGB TO FOO
+
+PROGRAM 1
+DECLARATIONS
+    ASSIGN REGA TO FOO
+
+    FOO = 1
+    REGA = 1
+
+PROGRAM 2
+    FOO = 2
+    REGB = 2
+"]
+      (is (= [:S
+              [:PROGRAM
+               [:PROGRAM_HEAD [:DEC "1"]]
+               [:PROGRAM_BODY
+                ;; Both statements use REGA (= program defs override global)
+                [:STATEMENT
+                 [:REG_ASSIGN [:REGISTER "A"] [:EXPR [:TERM [:HEX "1"]]]]]
+                [:STATEMENT
+                 [:REG_ASSIGN [:REGISTER "A"] [:EXPR [:TERM [:HEX "1"]]]]]]]
+              [:PROGRAM
+               [:PROGRAM_HEAD [:DEC "2"]]
+               [:PROGRAM_BODY
+                ;; Both statements use REGB (= program defs are scoped)
+                [:STATEMENT
+                 [:REG_ASSIGN [:REGISTER "B"] [:EXPR [:TERM [:HEX "2"]]]]]
+                [:STATEMENT
+                 [:REG_ASSIGN [:REGISTER "B"] [:EXPR [:TERM [:HEX "2"]]]]]]]]
+             (syms/process (src/p code))))))
