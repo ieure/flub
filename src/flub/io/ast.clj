3ca34ad2d1822df4e8477fa2a26af22a1e836e1d
Ian Eure <ian.eure@gmail.com>
2014-05-23 20:45:55 -0700

    Delete `io.ast` for now.

--- src/flub/io/ast.clj
+++
@@ -1,8 +0,0 @@
-;; -*- coding: utf-8 -*-
-;;
-;; © 2013 Buster Marx, Inc All rights reserved.
-;; Author: Ian Eure <ian.eure@gmail.com>
-;;
-(ns flub.io.ast)
-
-(defn ast->source "Turn an AST into source code")
