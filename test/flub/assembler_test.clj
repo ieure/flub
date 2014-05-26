149708947437a5f7a45263d41c830a6379c058d0
Ian Eure <ian.eure@gmail.com>
2014-05-25 18:36:08 -0700

    Test source

--- test/flub/assembler_test.clj
+++ test/flub/assembler_test.clj
@@ -180,75 +180,71 @@
            (format "Unhandled instructions in output: %s" unh#)))))
 
 (deftest test-available-source
+  (test-example "68000.s")
+  (test-example "9010A-TK80-Demo-Version30-Jun-81.S")
+
+  ;; ;; ISO-8859-1 hyphens in this
+  ;; #_(test-example "9010TEST.S")
+
+  ;; ;; "NOTE THAT THIS NEEDS EDITING..."
+  ;; #_(test-example "9010a-Workbook.S")
+
   (test-example "APPLE-II.S")
-  #_(test-example "opwolf.s")
-  )
+  ;; (test-example "ASTEROID-1.S") ;; Throws exception resolving syms
 
-;; (test-example "68000.s")
-;; (test-example "9010A-TK80-Demo-Version30-Jun-81.S")
+  ;; ;; BINARY
+  ;; #_(test-example "ASYNC.S")
 
-;; ;; ISO-8859-1 hyphens in this
-;; #_(test-example "9010TEST.S")
+  (test-example "After-Burner.s")
+  (test-example "BURNER.S")
+  (test-example "Beast-Busters.s")
+  (test-example "CHECK.S")
+  (test-example "CLEARPIT.S")
+  (test-example "DELAY.S")
+  (test-example "DEMO.S")
+  (test-example "FILE01.S")
+  (test-example "FILE02.S")
 
-;; ;; "NOTE THAT THIS NEEDS EDITING..."
-;; #_(test-example "9010a-Workbook.S")
+  ;; ;; Line hard wrapped -- not sure how to deal
+  ;; #_(test-example "FILE03.S")
 
-;; (test-example "APPLE-II.S")
-;; (test-example "ASTEROID-1.S")
+  (test-example "FILE04.S")
+  (test-example "FILE05.S")
+  (test-example "FILE06.S")
+  (test-example "FILE07.S")
+  (test-example "GALAGA.S")
+  (test-example "GuidedF.s")
+  (test-example "Hangon.s")
+  (test-example "ID-A-DAT.s")
+  (test-example "JIM.S")
+  (test-example "JOKER.S")
+  (test-example "JOKPOK.S")
+  (test-example "LELAND.S")
+  (test-example "LOOPPIT.S")
+  (test-example "LOOPPOLE.S")
+  (test-example "OperationWolf.s")
+  (test-example "PAC.S")
+  (test-example "PLAY.S")
+  (test-example "PLAY5.S")
+  (test-example "POD_TEST_6502_Rev1-0.S")
+  (test-example "POD_TEST_6800_Rev1-0.S")
+  (test-example "POD_TEST_8080_Rev1-1.S")
+  (test-example "POD_TEST_8085_Rev1_0.S")
+  (test-example "POD_TEST_Z80_Rev1-1.S")
+  (test-example "POLE.S")
+  (test-example "POT_TEST_6802_Rev1-0.S")
+  (test-example "Pac-corner-dots.s")
+  (test-example "Pac-test.s")
+  (test-example "RS232TST.S")
 
-;; ;; BINARY
-;; #_(test-example "ASYNC.S")
+  ;; ;; BINARY
+  ;; #_(test-example "Unknown tape.S")
 
-;; (test-example "After-Burner.s")
-;; (test-example "BURNER.S")
-;; (test-example "Beast-Busters.s")
-;; (test-example "CHECK.S")
-;; (test-example "CLEARPIT.S")
-;; (test-example "DELAY.S")
-;; (test-example "DEMO.S")
-;; (test-example "FILE01.S")
-;; (test-example "FILE02.S")
-
-;; ;; Line hard wrapped -- not sure how to deal
-;; #_(test-example "FILE03.S")
-
-;; (test-example "FILE04.S")
-;; (test-example "FILE05.S")
-;; (test-example "FILE06.S")
-;; (test-example "FILE07.S")
-;; (test-example "GALAGA.S")
-;; (test-example "GuidedF.s")
-;; (test-example "Hangon.s")
-;; (test-example "ID-A-DAT.s")
-;; (test-example "JIM.S")
-;; (test-example "JOKER.S")
-;; (test-example "JOKPOK.S")
-;; (test-example "LELAND.S")
-;; (test-example "LOOPPIT.S")
-;; (test-example "LOOPPOLE.S")
-;; (test-example "OperationWolf.s")
-;; (test-example "PAC.S")
-;; (test-example "PLAY.S")
-;; (test-example "PLAY5.S")
-;; (test-example "POD_TEST_6502_Rev1-0.S")
-;; (test-example "POD_TEST_6800_Rev1-0.S")
-;; (test-example "POD_TEST_8080_Rev1-1.S")
-;; (test-example "POD_TEST_8085_Rev1_0.S")
-;; (test-example "POD_TEST_Z80_Rev1-1.S")
-;; (test-example "POLE.S")
-;; (test-example "POT_TEST_6802_Rev1-0.S")
-;; (test-example "Pac-corner-dots.s")
-;; (test-example "Pac-test.s")
-;; (test-example "RS232TST.S")
-
-;; ;; BINARY
-;; #_(test-example "Unknown tape.S")
-
-;; (test-example "Z80-IO.S")
-;; (test-example "Z80.S")
-;; (test-example "beast.s")
-;; (test-example "blit.s")
-;; (test-example "blit2.s")
-;; ;; BINARY
-;; #_(test-example "fluke-tape.s")
-;; (test-example "opwolf.s")
+  (test-example "Z80-IO.S")
+  (test-example "Z80.S")
+  (test-example "beast.s")
+  (test-example "blit.s")
+  (test-example "blit2.s")
+  ;; ;; BINARY
+  ;; #_(test-example "fluke-tape.s")
+  (test-example "opwolf.s"))
